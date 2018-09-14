26d6707b72b8df57e51b601df54c2569ff82c33c
Travis T <travistokuyama@gmail.com>
2018-09-14 13:58:49 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,5 +18,10 @@ def take_a_number(katz_deli, name)
 end
 
 def now_serving(katz_deli)
-  puts "Currently serving #{katz_deli(0)}"
+  if katz_deli.length == 0 
+    puts "The line is currently empty"
+  else  
+    puts "Currently serving #{katz_deli(0)}"
+    katz_deli.shift()
+  end
 end
