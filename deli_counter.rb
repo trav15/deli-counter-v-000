cd1fc916cbfe6f9c8306fc09e0261af340c3e7dd
Travis T <travistokuyama@gmail.com>
2018-09-14 13:56:46 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,6 +17,6 @@ def take_a_number(katz_deli, name)
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
-def now_serving
-  line(katz_deli)
+def now_serving(katz_deli)
+  puts "Currently serving #{katz_deli(0)}"
 end
